9cf19e97d2e97605f1a69cbafe1c88d73f964dd2
desimonef <fdesimone@itba.edu.ar>
2023-09-14 22:22:24 -0300

    Pequeñisimo cambio

--- tpe1-g7/server/src/main/java/ar/edu/itba/pod/grpc/servants/NotifRequestsServant.java
+++ tpe1-g7/server/src/main/java/ar/edu/itba/pod/grpc/servants/NotifRequestsServant.java
@@ -76,7 +76,7 @@ public class NotifRequestsServant extends NotifRequestsServiceGrpc.NotifRequests
                                 return;
                             }
                             if(!repository.visitorHasPass(visitorId, day)){
-                                String msg = "Visitor " + visitorId + " already has pass for day " + day + ".";
+                                String msg = "Visitor " + visitorId + " doesn't have pass for day " + day + ".";
                                 logger.error(msg);
                                 responseObserver.onError(Status.INVALID_ARGUMENT.withDescription(msg).asRuntimeException());
                                 return;
